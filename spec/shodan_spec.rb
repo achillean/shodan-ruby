1cca4c7617463ab1b583c0be511106edb281e7c5
Sam <samuel@pagedegeek.com>
2014-02-24 10:57:08 +0100

    Spec for Shodan#base_url_for

--- spec/shodan_spec.rb
+++ spec/shodan_spec.rb
@@ -1,7 +1,14 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe "shodan" do
+describe Shodan::Shodan do
   it "should do nothing" do
     true.should == true
+  end
+
+  describe '#base_url_for' do
+    subject { Shodan::Shodan.new(nil) }
+    it { expect(subject.base_url_for(:exploits)).to eq(Shodan::BASE_URL[:exploits]) }
+    it { expect(subject.base_url_for(:main)).to eq(Shodan::BASE_URL[:main]) }
+    it { expect(subject.base_url_for(nil)).to eq(Shodan::BASE_URL[:main]) }
   end
 end
